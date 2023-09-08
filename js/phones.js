38a3c033ad47f7c00fc9a57b820feb62a795bef3
Apurba1903 <apurba1903@gmail.com>
2023-09-08 20:44:20 +0600

    added search bar

--- js/phones.js
+++ js/phones.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -12,13 +12,15 @@ const displayPhones = phones => {
 
     // Number 1: jeikhane boshabo oikhane place korsi id diye
     const phoneContainer = document.getElementById('phones-container');
+    // Clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
 
     phones.forEach(phone => {
         console.log(phone);
 
         // Number 2: div create korsi poshanor pore ei div e shob dekhabe
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.classList = `card bg-base-100 p-4 shadow-xl`;
 
         // Number 3: inner html set korte hobe shob kisu er vitore anar jonne
         phoneCard.innerHTML = `
@@ -39,7 +41,14 @@ const displayPhones = phones => {
     });
 }
 
+// Handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
 
 
 
-loadPhone();
+// loadPhone();
